516a79c9312e5329b5939850622b5880b5984355
DAITHANG23 <nguyendaithang23061997@gmail.com>
2023-07-28 00:15:26 +0700

    update code

--- project-tranning-2/src/index.tsx
+++ project-tranning-2/src/index.tsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
-import customTheme from "@/themes/theme";
 import { BrowserRouter } from "react-router-dom";
+import theme from "@/themes/theme";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={customTheme}>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
