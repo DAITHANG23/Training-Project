5240e311ac2ee7c26d29938587104c25902edde1
DAITHANG23 <nguyendaithang23061997@gmail.com>
2023-08-06 09:09:21 +0700

    update

--- project-training-2/src/index.tsx
+++ project-training-2/src/index.tsx
@@ -5,9 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/themes/theme";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
